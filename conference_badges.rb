8992747fb960aa05b25456af4f3ad5e963a9628b
Eugene <eugeneayonga@gmail.com>
2022-09-04 11:33:48 +0300

    commit tree 8aea70f93c3cff1cc14391e33f26630af222260a

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+# Create a #badge_maker method
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    array_new = []
+    array.each do |name|
+        array_new << "Hello, my name is #{name}."
+    end
+    array_new
+end
+
+def assign_rooms(array)
+    array_new = []
+    array.each_with_index do |name, index|
+        array_new << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+    end
+    array_new
+end
+
+def printer(array)
+    batch_badge_creator(array).each do |badge|
+        puts badge
+    end
+    assign_rooms(array).each do |room|
+        puts room
+    end
+end
